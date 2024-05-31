80199167501cc393be8502829202c871ea1e7e57
namvinhkhang <83082969+namvinhkhang@users.noreply.github.com>
2024-05-31 12:03:04 +0700

    Add files via upload

---
+++ Data Visualizer.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.impute import SimpleImputer
+import umassScrape
+
+
+# different_utils = umassScrape.run()
+different_utils = ['Gas', 'Water', 'Electricity', 'Heat', 'Trash Removal',
+                   'Sewer', 'Air Conditioning', 'Internet', 'Cable']
+# Load the data
+X_full = pd.read_csv('housing_data.csv')
+
+#Ploting Data
+sns.set_theme()
+data = X_full.groupby('Size (square Feet)')['Price/Bed'].mean().reset_index()
+sns.barplot(x = "Size (square Feet)", y = "Price/Bed", data = data)
+
+plt.show()
+
+
+#Transform the Included Util column
+for util in different_utils:
+    X_full[util] = 0
+
+for index, row in X_full.iterrows():
+    included_util = row['Included Utilities']
+    if included_util is np.nan:
+        included_util = ""
+    included_util.strip()
+    included_util = included_util.split(", ")
+    for util in included_util[:len(included_util) - 1]:
+        X_full.at[index, util] = 1
+
+X_full.drop('Included Utilities', axis=1, inplace = True)
+
+X_full.dropna(axis = 0, subset = ['Price/Bed'], inplace = True)
+y = X_full['Price/Bed']
+
+info_cols = ["Link", "Phone Number", "Name", "Address", "Price/Bed"]
+X_full.drop(info_cols, axis = 1, inplace = True)
+
+X_train, X_valid, y_train, y_valid = train_test_split(X_full, y, train_size=0.8, test_size=0.2,
+                                                      random_state=0)
+
+#Impute Missing Data
+this_imputer = SimpleImputer(strategy = 'mean')
+
+imputed_X_train = pd.DataFrame(this_imputer.fit_transform(X_train, y))
+imputed_X_valid = pd.DataFrame(this_imputer.transform(X_valid))
+
+imputed_X_train.columns = X_train.columns
+imputed_X_valid = X_valid.columns
+
+model = RandomForestRegressor(n_estimators = 200, random_state = 0)
+model.fit(X_train, y_train)
+pred = model.predict(X_valid)
+print(mean_absolute_error(y_valid, pred))
+
--- umassScrape.py
+++ umassScrape.py
@@ -1,136 +1,158 @@
-import pprint
 import operator
+import csv
 import math
+import numpy as np
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-#INITIALIZED THE DRIVER TO SCRAPE THE WEBSITE
-driver = webdriver.Chrome()
-driver.get('https://offcampushousing.umass.edu/housing')
-
-#CREATE A SOUP OF THE WEB
-soup = BeautifulSoup(driver.page_source, 'html.parser')
-num_place = int((soup.find('p',
-                          attrs = {'class': 'list-search-results-available-count'})).text.split()[0])
-
-#TAKE IN ALL THE LISTING SOUP IN THE THE LIST "LISTINGS"
-listings = []
-i = 1
-while i <= math.ceil(num_place / 40):
-    url = 'https://offcampushousing.umass.edu/housing/page-' + f'{i}'
-    driver.get(url)
-    wait = WebDriverWait(driver, 10)
-    wait.until(EC.presence_of_all_elements_located((By.TAG_NAME, 'body')))
-    temp_soup = BeautifulSoup(driver.page_source, 'html.parser')
-    listings.extend(temp_soup.find_all('li', attrs = {'class': 'list-result-item'}))
-    i += 1
-
-# print(len(listings))
-houses = []
-
-#Ways to sort
-price_comparator = operator.attrgetter('price')
-distant_comparator = operator.attrgetter('distant_from_campus')
-num_bed_comparator = operator.attrgetter('num_bed')
-
-#Link prefix
-LINK_PREFIX = "https://offcampushousing.umass.edu"
-
-for house in listings:
-    #Contact INFO
-    phone_number = house.find_all('span', attrs = {'class': 'hidden sm:inline'})[-1].text
-
-    distant_from_campus = house.find('span', attrs = {'class': 'bold'}).text.split()[0]
-    distant_from_campus = distant_from_campus.replace("+", "")
-    distant_from_campus = float(distant_from_campus)
-
-    #Link to the Listing itself
-    link = LINK_PREFIX + house.find('a', attrs = {'class': 'card-anchor text-wrap'}).get('href')
-    #Scrapping more details about the House
-    driver.get(link)
-    sub_soup = BeautifulSoup(driver.page_source, 'html.parser')
-
-    name = sub_soup.h1.text
-    
-    address = sub_soup.find('p', attrs = {'data-qaid': 'address'})
-    if address:
-        address = address.text
-    else:
-        address = "N/A"
-
-    floor_plan_options = sub_soup.find_all('tr', attrs = {'data-qaid': 'floorPlanRow'})
-    del floor_plan_options[1::2] #Remove the hidden css that is meant for Mobile User
-
-    util_include = "N/A"
-    raw_util_included = sub_soup.find('table', attrs = {'class': 'expenses-table',
-                                                    'data-qaid': 'utilities'})
-    if raw_util_included:
-        raw_util_included = raw_util_included.find_all('td', attrs = {'data-qaid': 'name'})
-        #A list of utilities included in the Listing
-        util_include = [util.text for util in raw_util_included]
-
-    for option in floor_plan_options:
-        num_bed = option.find('td', attrs = {'data-qaid' : 'beds'}).text
-        num_bed = num_bed.split()[0]
-        if num_bed != "Studio":
-            num_bed = int(num_bed)
-
-        num_bath = float(option.find('td', attrs = {'data-qaid': 'baths'}).text.split()[0])
+def run():
+    driver = webdriver.Chrome()
+    driver.get('https://offcampushousing.umass.edu/housing')
 
 
-        whole_price_str = option.find('td', attrs = {'data-qaid' : 'price'}).text
-        whole_price_str = whole_price_str.replace("$", "")
-        whole_price_str = whole_price_str.replace(",", "")
-        whole_price_str = whole_price_str.split()
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    num_place = int((soup.find('p',
+                            attrs = {'class': 'list-search-results-available-count'})).text.split()[0])
 
-        price = whole_price_str[-1]
+    listings = []
+    i = 1
+    while i <= math.ceil(num_place / 40):
+        url = 'https://offcampushousing.umass.edu/housing/page-' + f'{i}'
+        driver.get(url)
+        wait = WebDriverWait(driver, 10)
+        wait.until(EC.presence_of_all_elements_located((By.TAG_NAME, 'body')))
+        temp_soup = BeautifulSoup(driver.page_source, 'html.parser')
+        listings.extend(temp_soup.find_all('li', attrs = {'class': 'list-result-item'}))
+        i += 1
 
-        for i, s in enumerate(whole_price_str):
-            if s == "/":
-                price = whole_price_str[i - 1]
-                break
+    print(len(listings))
+    houses = []
 
-        try:
-            price = int(price)
-            if num_bed == 'Studio':
-                price = math.ceil(price)
+    #Ways to sort
+    price_comparator = operator.attrgetter('price')
+    distant_comparator = operator.attrgetter('distant_from_campus')
+    num_bed_comparator = operator.attrgetter('num_bed')
+
+    diffrent_util = {}
+    #Link prefix
+    LINK_PREFIX = "https://offcampushousing.umass.edu"
+
+    for house in listings:
+        #Contact INFO
+        phone_number = house.find_all('span', attrs = {'class': 'hidden sm:inline'})[-1].text
+
+        distant_from_campus = house.find('span', attrs = {'class': 'bold'}).text.split()[0]
+        distant_from_campus = distant_from_campus.replace("+", "")
+        distant_from_campus = float(distant_from_campus)
+
+        #Link to the Listing itself
+        link = LINK_PREFIX + house.find('a', attrs = {'class': 'card-anchor text-wrap'}).get('href')
+        #Scrapping more details about the House
+        driver.get(link)
+        sub_soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+
+        name = sub_soup.h1
+        if name:
+            name = name.text
+        else:
+            name = np.nan
+
+        address = sub_soup.find('p', attrs = {'data-qaid': 'address'})
+        if address:
+            address = address.text
+        else:
+            address = np.nan
+
+        floor_plan_options = sub_soup.find_all('tr', attrs = {'data-qaid': 'floorPlanRow'})
+        del floor_plan_options[1::2] #Remove the hidden css that is meant for Mobile User
+
+        util_include = ""
+        raw_util_included = sub_soup.find('table', attrs = {'class': 'expenses-table',
+                                                        'data-qaid': 'utilities'})
+        if raw_util_included:
+            raw_util_included = raw_util_included.find_all('td', attrs = {'data-qaid': 'name'})
+            #A list of utilities included in the Listing
+            for util in raw_util_included:
+                diffrent_util[util.text] = 1
+                util_include += util.text + ", "
+        for option in floor_plan_options:
+            num_bed = option.find('td', attrs = {'data-qaid' : 'beds'}).text
+            num_bed = num_bed.split()[0]
+            is_Studio = 0
+            if num_bed != "Studio":
+                num_bed = int(num_bed)
             else:
+                num_bed = 1
+                is_Studio = 1
+
+            num_bath = float(option.find('td', attrs = {'data-qaid': 'baths'}).text.split()[0])
+
+
+            whole_price_str = option.find('td', attrs = {'data-qaid' : 'price'}).text
+            whole_price_str = whole_price_str.replace("$", "")
+            whole_price_str = whole_price_str.replace(",", "")
+            whole_price_str = whole_price_str.split()
+
+            price = whole_price_str[-1]
+
+            for i, s in enumerate(whole_price_str):
+                if s == "/":
+                    price = whole_price_str[i - 1]
+                    break
+
+            try:
+                price = int(price)
                 price = math.ceil(price / num_bed)
-        except:
-            price = "N/A"
+            except:
+                price = np.nan
 
-        sq_feet = option.find('td', attrs = {'data-qaid' : 'sqFeet'})
-        if sq_feet:
-            sq_feet = sq_feet.text
+            sq_feet = option.find('td', attrs = {'data-qaid' : 'sqFeet'})
             if sq_feet:
-                sq_feet = sq_feet.replace(",", '')
-                sq_feet = int(sq_feet.split()[-1])
-        else:
-            sq_feet = "N/A"
+                sq_feet = sq_feet.text
+                if sq_feet:
+                    sq_feet = sq_feet.replace(",", '')
+                    sq_feet = int(sq_feet.split()[-1])
+            else:
+                sq_feet = np.nan
 
-        max_occu = option.find('td', attrs = {'data-qaid' : 'maxOccupants'})
-        if max_occu:
-            max_occu = max_occu.text.split()
+            max_occu = option.find('td', attrs = {'data-qaid' : 'maxOccupants'})
             if max_occu:
-                max_occu = int(max_occu[0])
-        else:
-            max_occu = "N/A"
+                max_occu = max_occu.text.split()
+                if max_occu:
+                    max_occu = int(max_occu[0])
+                else:
+                    max_occu = np.nan
+            else:
+                max_occu = np.nan
 
-        houses.append({
-            'Link': link,
-            'Phone Number': phone_number,
-            'Name': name,
-            'Address': address,
-            'Distant From Campus': distant_from_campus,
-            'Bedroom(s) / Studio': num_bed,
-            'Bathroom(s)': num_bath,
-            'Included Utilities': util_include,
-            'Price Ber Bed': price,
-            'Size (square feet)': sq_feet,
-            'MAXIMUM Number of People': max_occu,
-        })
+            houses.append({
+                'Link': link,
+                'Phone Number': phone_number,
+                'Name': name,
+                'Address': address,
+                'Distant From Campus': distant_from_campus,
+                'Bedroom(s)': num_bed,
+                'Is Studio?': is_Studio,
+                'Bathroom(s)': num_bath,
+                'Included Utilities': util_include,
+                'Price/Bed': price,
+                'Size (square Feet)': sq_feet,
+                'MAXIMUM Number of People': max_occu,
+            })
 
-# print(num_place, len(houses))
+    print(num_place, len(houses))
+
+    #Write to a CSV File to store the data for future usage.
+    keys = houses[0].keys()
+
+    with open('housing_data.csv', 'w', newline='', encoding="utf-8") as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(houses)
+
+    print(diffrent_util.keys())
+    return(list(diffrent_util.keys()))
